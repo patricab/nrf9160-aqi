106dd42718dbcedeef62982f0ee02ce90cdeb162
ejaanens <ejaanens@stud.ntnu.no>
2021-03-14 16:09:42 +0100

    sensor drafts

--- src/main.c
+++ src/main.c
@@ -40,7 +40,7 @@ BUILD_ASSERT(sizeof(CONFIG_APP_LWM2M_SERVER) > 1,
 #define APP_BANNER "Run LWM2M client"
 
 #define IMEI_LEN		15
-#define ENDPOINT_NAME_LEN	(12)
+#define ENDPOINT_NAME_LEN	sizeof(CONFIG_APP_LWM2M_CLIENT)
 
 #define LWM2M_SECURITY_PRE_SHARED_KEY 0
 #define LWM2M_SECURITY_RAW_PUBLIC_KEY 1
@@ -443,7 +443,7 @@ void main(void)
 	/* query IMEI */
 	query_modem("AT+CGSN", imei_buf, sizeof(imei_buf));
 	/* use IMEI as unique endpoint name */
-	snprintf(endpoint_name, sizeof(endpoint_name), "nrf-9160dk-e"); //%s", imei_buf);
+	snprintf(endpoint_name, sizeof(endpoint_name), "%s", CONFIG_APP_LWM2M_CLIENT); //%s", imei_buf);
 	LOG_INF("endpoint: %s", log_strdup(endpoint_name));
 
 	/* Setup LwM2M */
