3e52905de9994184d428a651e2a84247b43e6ae6
patricab <patric.berthelsen@gmail.com>
2021-03-17 12:55:41 +0100

    * Working temperature measurements * Added support for BME680 - thingy91_nrf9160ns.overlay

--- src/lwm2m/lwm2m_temp.c
+++ src/lwm2m/lwm2m_temp.c
@@ -7,6 +7,8 @@
 #include <zephyr.h>
 #include <drivers/sensor.h>
 #include <net/lwm2m.h>
+#include <device.h>
+#include <stdio.h>
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app_lwm2m_temp, CONFIG_APP_LOG_LEVEL);
@@ -14,35 +16,35 @@ LOG_MODULE_REGISTER(app_lwm2m_temp, CONFIG_APP_LOG_LEVEL);
 /* use 25.5C if no sensor available */
 static struct float32_value temp_float = { 25, 500000 };
 static const struct device *die_dev;
+/* const struct device  */
+
 static int32_t timestamp;
 
-#if defined(CONFIG_TEMP_NRF5_NAME)
 static int read_temperature(const struct device *temp_dev,
 			    struct float32_value *float_val)
 {
-	const char *name = temp_dev->config->name;
-	struct sensor_value temp_val;
+	/* const char *name = temp_dev->config->name; */
+	struct sensor_value temp;
 	int ret;
 
 	ret = sensor_sample_fetch(temp_dev);
 	if (ret) {
-		LOG_ERR("%s: I/O error: %d", name, ret);
+		LOG_ERR("BME680: I/O error: %d", ret);
 		return ret;
 	}
 
-	ret = sensor_channel_get(temp_dev, SENSOR_CHAN_DIE_TEMP, &temp_val);
+	ret = sensor_channel_get(temp_dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
 	if (ret) {
-		LOG_ERR("%s: can't get data: %d", name, ret);
+		LOG_ERR("BME680: can't get data: %d", ret);
 		return ret;
 	}
 
-	LOG_DBG("%s: read %d.%d C", name, temp_val.val1, temp_val.val2);
-	float_val->val1 = temp_val.val1;
-	float_val->val2 = temp_val.val2;
+	/* LOG_DBG("%s: read %d.%d C", name, temp.val1, temp.val2); */
+	float_val->val1 = temp.val1;
+	float_val->val2 = temp.val2;
 
 	return 0;
 }
-#endif
 
 static void *temp_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_inst_id,
 			  size_t *data_len)
@@ -55,15 +57,7 @@ static void *temp_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_in
 		return NULL;
 	}
 
-#if defined(CONFIG_TEMP_NRF5_NAME)
-	/*
-	 * No need to check if read was successful, just reuse the
-	 * previous value which is already stored at temp_float.
-	 * This is because there is currently no way to report read_cb
-	 * failures to the LWM2M engine.
-	 */
 	read_temperature(die_dev, &temp_float);
-#endif
 	lwm2m_engine_set_float32("3303/0/5700", &temp_float);
 	*data_len = sizeof(temp_float);
 	/* get current time from device */
@@ -76,12 +70,13 @@ static void *temp_read_cb(uint16_t obj_inst_id, uint16_t res_id, uint16_t res_in
 
 int lwm2m_init_temp(void)
 {
-#if defined(CONFIG_TEMP_NRF5_NAME)
-	die_dev = device_get_binding(CONFIG_TEMP_NRF5_NAME);
-	LOG_INF("%s on-die temperature sensor %s",
-		die_dev ? "Found" : "Did not find", CONFIG_TEMP_NRF5_NAME);
-#endif
+/* #if defined(CONFIG_TEMP_NRF5_NAME) */
+/* 	die_dev = device_get_binding(CONFIG_TEMP_NRF5_NAME); */
+/* 	LOG_INF("%s on-die temperature sensor %s", */
+/* 		die_dev ? "Found" : "Did not find", CONFIG_TEMP_NRF5_NAME); */
+/* #endif */
 
+   die_dev = device_get_binding(DT_LABEL(DT_INST(0, bosch_bme680)));
 	if (!die_dev) {
 		LOG_ERR("No temperature device found.");
 	}
