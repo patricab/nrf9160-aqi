56070b2a9fafbecccd7d455473c91f1b5bd9cd1d
patricab <patric.berthelsen@gmail.com>
2021-04-07 12:48:06 +0200

    WIP

--- bench/dk/dk.c
+++ bench/dk/dk.c
@@ -1,54 +1,65 @@
 #include "dgso3.h"
-#include <dk_buttons_and_leds.h>
 
-LOG_MODULE_REGISTER(button, CONFIG_APP_LOG_LEVEL);
+#include "ui.h"
+
+LOG_MODULE_REGISTER(dk, CONFIG_APP_LOG_LEVEL);
 
 static int16_t *rx_val;
 static const struct device *die_dev;
 
-static void btn_cb(uint32_t button_state, uint32_t has_changed) {
-    button_state = dk_get_buttons(); // Read current button states
+// Button state callback
+static void btn_cb(struct ui_evt *evt) {
+    int err;
 
-    if ((button_state & DK_BTN1_MSK) == DK_BTN1_MSK) // Check if button 1 has been pressed
+    if (!evt) {
+        return;
+    }
+    // Check if/which button is active
+    if (evt->type == UI_EVT_BUTTON_ACTIVE)
     {
-        has_changed = DK_BTN1_MSK;
-        int err = read_gas(rx_val);
-        if (err == 1)
+        switch (evt->button)
         {
-            LOG_ERR("Error: could not read measurement");
+            case UI_BUTTON_1:
+                err = read_gas(rx_val);
+                if (err == 1)
+                {
+                    LOG_ERR("Error: could not read measurement");
+                }
+                break;
+            case UI_BUTTON_2:
+                standby_gas();
+                break;
+            case UI_SWITCH_1:
+                zero_gas();
+                break;
         }
-    }
-    else if ((button_state & DK_BTN2_MSK) == DK_BTN2_MSK) // Check if button 2 has been pressed
-    {
-        has_changed = DK_BTN2_MSK;
-        standby_gas();
-    }
-    else if ((button_state & DK_BTN3_MSK) == DK_BTN3_MSK) // Check if button 3 has been pressed
-    {
-        has_changed = DK_BTN3_MSK;
-        zero_gas();
     }
 }
 
 // Initalize buttons library and wait for button state change
 void main(void) {
+    // Configure device
 	die_dev = device_get_binding(DT_LABEL(DT_NODELABEL(uart0)));
 	if (!die_dev)
 	{
 		LOG_ERR("No device found.");
 	}
 
+    // Configure UART
 	int err = init_uart(die_dev);
 	if (err == 1)
 	{
-		LOG_ERR("I/O error");
+		LOG_ERR("Error: Could not initialize UART");
 	}
 
-    err = dk_buttons_init(btn_cb);
-    if (err == 1) {
-        LOG_ERR("Error: Could not initalize buttons");
+    // Configure buttons
+    err = ui_init(btn_cb);
+    if (err < 0)
+    {
+        LOG_ERR("Error: could not initalize button 1");
     }
 
+    // Wait for input
     while (1) {
             k_cpu_idle();
         }
