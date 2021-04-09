e89a79aca551eb4f312f8b6559c5fba13d2f21da
patricab <patric.berthelsen@gmail.com>
2021-04-09 14:43:53 +0200

    * Modified header file

--- src/lib/sps30_I2C.h
+++ src/lib/sps30_I2C.h
@@ -14,13 +14,9 @@
 #define SPS_CMD_WAKE_UP 0x1103
 #define SPS_CMD_DELAY_USEC 5000
 #define SPS_CMD_DELAY_WRITE_FLASH_USEC 20000
-
 #define SPS30_I2C_ADDRESS 0x69
 #define SPS30_MAX_SERIAL_LEN 32
-/* 1s measurement intervals */
-#define SPS30_MEASUREMENT_DURATION_USEC 1000000
-/* 100ms delay after resetting the sensor */
-#define SPS30_RESET_DELAY_USEC 100000
+
 /** The fan is switched on but not running */
 #define SPS30_DEVICE_STATUS_FAN_ERROR_MASK (1 << 4)
 /** The laser current is out of range */
@@ -28,19 +24,28 @@
 /** The fan speed is out of range */
 #define SPS30_DEVICE_STATUS_FAN_SPEED_WARNING (1 << 21)
 
-int sps30_init(const struct device *dev);
-
-int sps30_set_pointer(const struct device *dev, uint16_t addr);
-
-int sps30_i2c_read(const struct device *dev,uint16_t addr, uint8_t *data, uint32_t num_bytes);
-
-int sps30_i2c_write(const struct device *dev, uint16_t addr, uint8_t *data, uint32_t num_bytes);
-
-int sps30_particle_read(const struct device *pms_dev,uint16_t addr, uint8_t *data, uint32_t num_bytes);
-
 struct sps30_data
 {
 	uint32_t nc_2p5; // Number concentration PM2.5 [#/cm^3]
 	uint32_t nc_10p0; // Number concentration PM10 [#/cm^3]
 	uint32_t typ_size; // Typical Particle Size [nm]
 }sps30;
+
+/**
+	@brief Initialize sps30 library
+
+	@param dev Pointer to user device
+
+	@retval 0 if successful, 1 if errors occured
+*/
+int sps30_init(const struct device *dev)
+
+/**
+	@brief Read particle measurment
+
+	@param dev Pointer to user device
+	@param sps30_data Pointer to sensor struct
+
+	@retval 0 if successful, 1 if errors occured
+*/
+int sps30_particle_read(const struct device *dev, struct *sps30_data)
