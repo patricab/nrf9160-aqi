5156db2545437427c2c4ad96ba12d5d9c582a8cf
patricab <patric.berthelsen@gmail.com>
2021-05-06 12:33:19 +0200

    Update for stat.py: * Updated statistical functions * Added comments for printouts * Added comments

--- stat.py
+++ stat.py
@@ -15,25 +15,33 @@ import scipy.stats as st
 #         valid - Bool flag: True when p value < 0.05, which rejects null hypothesis (data is correlated)
 #                            False when p value > 0.05, which accepts null hypothesis (data not correlected)
 class Valid:
-    def __init__(self, file1, file2):
+    def __init__(self, file1, file2, length):
+
         # Read CSV files, extract values column into numpy array
         df1 = pd.read_csv(file1, header=0, parse_dates=True)
         df2 = pd.read_csv(file2, header=0, parse_dates=True)
-        val1 = df1.iloc[:, 6].to_numpy()
-        val2 = df2.iloc[:, 6].to_numpy()
+        val1 = df1.iloc[0:length, 6].to_numpy()
+        val2 = df2.iloc[0:length, 6].to_numpy()
 
-        # Set class 'r' and 'p' values
+        if (len(val1) != len(val2)):
+            sys.exit("Length of arrays must be the same")
+
+        # Set class 'r', 't' and 'p' values, as well as validity tests
         # print(st.pearsonr(val, val))
-        self.r = st.pearsonr(val1, val2)[0]
-        self.p = st.t.sf(abs(r), df=2)
+        self.r = st.pearsonr(val1, val2)[0] 
+        self.valid1 = abs(self.r) >= 2/math.sqrt(length) # First test - Check pearson correlation rule of thumb
+        self.t = r*math.sqrt((length - 2)/(1 - r**2)) # Get T value from student T-distrubution of pearson correlation
+        self.p = st.t.sf(abs(self.t), df=2) # Lookup p value based on t value
 
-        if (self.p < 0.05): # Check if p value is less than alpha level => rejects null hypothesis
-            self.valid = True
+        if (self.p <= 0.05): # Check if p value is less than alpha level => rejects null hypothesis
+            self.valid2 = True
         else: # Check if p value is greater than alpha level => accept null hypothesis
-            self.valid = False
+            self.valid2 = False
             
 # %%
-t = Valid("test.csv","test.csv")
-print(t.r)
-print(t.p)
-print(t.valid)
+t = Valid("test.csv","test.csv", 10)
+print("r value: ", t.r)
+print("t value: ", t.t)
+print("p value: ", t.p)
+print("Test1 (Pearson RoT): ", t.valid1)
+print("Test2 (T-test): ", t.valid2)
